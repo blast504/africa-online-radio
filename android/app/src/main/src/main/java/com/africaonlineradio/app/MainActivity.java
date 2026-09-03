54cb1059dba9663cbe647a4b61936352bec4f0fe
blast504 <yassinm502@gmail.com>
2026-09-03 12:59:19 +0200

    Update MainActivity.java

--- android/app/src/main/src/main/java/com/africaonlineradio/app/MainActivity.java
+++ android/app/src/main/src/main/java/com/africaonlineradio/app/MainActivity.java
@@ -1,1 +1,38 @@
+package com.africaonlineradio.app;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("https://blast504.github.io/africa-online-radio/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
